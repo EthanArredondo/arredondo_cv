48de34917a0d7604d3d87265bc3ff57336ec603b
ethan arredondo <ethansarredondo@gmail.com>
2024-12-15 20:11:48 -0500

    creación de estructura html

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Principal from './views/Principal';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Principal />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/Principal.js
@@ -0,0 +1,127 @@
+import React from "react";
+
+function Principal() {
+  return (
+    <div className="Principal">
+      <section className="presentacion">
+        <div className="contenido">
+          <header></header>
+          <div className="titulos">
+            <h1>ETHAN ARREDONDO</h1>
+            <h2>Diseñador y Desarrollador de Software</h2>
+          </div>
+        </div>
+      </section>
+      <section className="info_personal">
+        <div className="contenido">
+          <h3>Sobre mi</h3>
+          <h2>PERFIL PROFESIONAL</h2>
+          <p>Egresado de la carrera Diseño y Desarrollo de Software en Tecsup.</p>
+          <p>Destacado por liderar equipos, adaptarse rápidamente a nuevos desafíos y mantener una actitud proactiva y positiva bajo presión. Poseé habilidades de comunicación efectiva y capacidad para aprender rápidamente, con el objetivo de aportar soluciones innovadoras y eficientes</p>
+        </div>
+      </section>
+      <section className="habilidades">
+        <div className="contenido">
+          <h3>Mis habilidades</h3>
+          <h2>ME ESPECIALIZO EN</h2>
+          <div className="fila">
+            <div className="columna">
+              <h4>HTML/CSS</h4>
+              <div className="barra_progreso">
+                <div className="barra"></div>
+              </div>
+            </div>
+            <div className="columna">
+              <h4>JAVASCRIPT</h4>
+              <div className="barra_progreso">
+                <div className="barra"></div>
+              </div>
+            </div>
+          </div>
+          <div className="fila">
+            <div className="columna">
+              <h4>PYTHON</h4>
+              <div className="barra_progreso">
+                <div className="barra"></div>
+              </div>
+            </div>
+            <div className="columna">
+              <h4>JAVA</h4>
+              <div className="barra_progreso">
+                <div className="barra"></div>
+              </div>
+            </div>
+          </div>
+          <div className="fila">
+            <div className="columna">
+              <h4>PHP</h4>
+              <div className="barra_progreso">
+                <div className="barra"></div>
+              </div>
+            </div>
+            <div className="columna">
+              <h4>MYSQL</h4>
+              <div className="barra_progreso">
+                <div className="barra"></div>
+              </div>
+            </div>
+          </div>
+          <h2>OTRAS HABILIDADES</h2>
+          <div className="fila">
+            <div className="columna">
+              <h4>PHOTOSHOP</h4>
+              <div className="barra_progreso">
+                <div className="barra"></div>
+              </div>
+            </div>
+            <div className="columna">
+              <h4>ILLUSTRATOR</h4>
+              <div className="barra_progreso">
+                <div className="barra"></div>
+              </div>
+            </div>
+          </div>
+          <div className="fila">
+            <div className="columna">
+              <h4>BIZAGI</h4>
+              <div className="barra_progreso">
+                <div className="barra"></div>
+              </div>
+            </div>
+            <div className="columna">
+              <h4>MONGODB</h4>
+              <div className="barra_progreso">
+                <div className="barra"></div>
+              </div>
+            </div>
+          </div>
+          <div className="fila">
+            <div className="columna">
+              <h4>PHOTOSHOP</h4>
+              <div className="barra_progreso">
+                <div className="barra"></div>
+              </div>
+            </div>
+            <div className="columna">
+              <h4>ILLUSTRATOR</h4>
+              <div className="barra_progreso">
+                <div className="barra"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="certificados">
+        <div className="contenido"></div>
+      </section>
+      <section className="experiencia">
+        <div className="contenido"></div>
+      </section>
+      <section className="proyectos">
+        <div className="contenido"></div>
+      </section>
+    </div>
+  );
+}
+
+export default Principal;
