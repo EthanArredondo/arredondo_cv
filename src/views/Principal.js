02a1ce541602c052ea696ce544df4eacf4d6a448
ethan arredondo <ethansarredondo@gmail.com>
2024-12-23 18:12:09 -0500

    Finalización de los bosquejos

--- src/views/Principal.js
+++ src/views/Principal.js
@@ -2,7 +2,7 @@ import React from "react";
 import '../styles/Principal.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram, faYoutube, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faBars } from '@fortawesome/free-solid-svg-icons'; // Importa un ícono específico
+import { faBars, faUser, faLocation, faFlag } from '@fortawesome/free-solid-svg-icons'; // Importa un ícono específico
 
 
 function Principal() {
@@ -44,6 +44,23 @@ function Principal() {
           <h2>PERFIL PROFESIONAL</h2>
           <p className="especial">Egresado de la carrera Diseño y Desarrollo de Software en Tecsup.</p>
           <p>Destacado por liderar equipos, adaptarse rápidamente a nuevos desafíos y mantener una actitud proactiva y positiva bajo presión. Poseé habilidades de comunicación efectiva y capacidad para aprender rápidamente, con el objetivo de aportar soluciones innovadoras y eficientes</p>
+          <div class="fila">
+            <div class="col">
+              <i><FontAwesomeIcon icon={faUser} /></i>
+              <span class="titulo-detalle">Perfil</span>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est a debitis sed quis quasi neque, autem aut, rerum et dolorum omnis, voluptatibus quibusdam quae minima explicabo obcaecati. Quod, corrupti similique?</p>
+            </div>
+            <div class="col">
+              <i><FontAwesomeIcon icon={faLocation} /></i>
+              <span class="titulo-detalle">Ubicación</span>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est a debitis sed quis quasi neque, autem aut, rerum et dolorum omnis, voluptatibus quibusdam quae minima explicabo obcaecati. Quod, corrupti similique?</p>
+            </div>
+            <div class="col">
+              <i><FontAwesomeIcon icon={faFlag} /></i>
+              <span class="titulo-detalle">Intereses</span>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est a debitis sed quis quasi neque, autem aut, rerum et dolorum omnis, voluptatibus quibusdam quae minima explicabo obcaecati. Quod, corrupti similique?</p>
+            </div>
+          </div>
         </div>
       </section>
       <section className="habilidades">
@@ -159,6 +176,82 @@ function Principal() {
         <div className="contenido">
           <h3>Resumen</h3>
           <h2>PREPARACIÓN Y EXPERIENCIAS</h2>
+          <div className="info">
+            <div class="col">
+              <span class="titulo">Educación</span>
+              <table>
+                <tr>
+                  <td class="datos">
+                    <h3>2006-2010</h3>
+                  </td>
+                  <td rowspan="2" class="descripción">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente eaque pariatur reiciendis. Laboriosam adipisci eos eaque, at culpa, minima nostrum quam odit blanditiis, architecto voluptatem. Quaerat libero unde odit quos!
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4>Lic. en computación</h4>
+                    <p>Universidad de San Juan</p>
+                    <span class="promedio">8.9PG</span>
+                  </td>
+                </tr>
+              </table>
+              <table>
+                <tr>
+                  <td class="datos">
+                    <h3>2010-2015</h3>
+                  </td>
+                  <td rowspan="2" class="descripción">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente eaque pariatur reiciendis. Laboriosam adipisci eos eaque, at culpa, minima nostrum quam odit blanditiis, architecto voluptatem. Quaerat libero unde odit quos!
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4>Programador FS</h4>
+                    <p>Udemy Cursos</p>
+                    <span class="promedio">8.9PG</span>
+                  </td>
+                </tr>
+              </table>
+            </div>
+            <div class="col">
+              <span class="titulo">Experiencia Laboral</span>
+              <table>
+                <tr>
+                  <td class="datos">
+                    <h3>3 años</h3>
+                  </td>
+                  <td rowspan="2" class="descripción">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente eaque pariatur reiciendis. Laboriosam adipisci eos eaque, at culpa, minima nostrum quam odit blanditiis, architecto voluptatem. Quaerat libero unde odit quos!
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4>Mercado Libre</h4>
+                    <p>Programador Junior</p>
+                    <span class="promedio">8.9PG</span>
+                  </td>
+                </tr>
+              </table>
+              <table>
+                <tr>
+                  <td class="datos">
+                    <h3>2 años</h3>
+                  </td>
+                  <td rowspan="2" class="descripción">
+                    Loredsdsdm, ipsum dolor sit amet consectetur adipisicing elit. Sapiente eaque pariatur reiciendis. Laboriosam adipisci eos eaque, at culpa, minima nostrum quam odit blanditiis, architecto voluptatem. Quaerat libero unde odit quos!
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4>Software inc. FS</h4>
+                    <p>Diseñador principal</p>
+                    <span class="promedio">8.9PG</span>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </div>
         </div>
       </section>
       <section className="certificados">
@@ -174,6 +267,32 @@ function Principal() {
         <div className="contenido">
           <h3>Proyectos personales</h3>
           <h2>PROYECTOS PERSONALES DESARROLLADOS</h2>
+        </div>
+      </section>
+      <section className="contacto">
+        <div className="contenido">
+          <h2 class="titulo-seccion">Contacto</h2>
+          <h3>Ponte en contacto ahora mismo!</h3>
+          <div class="fila">
+            <div class="col">
+              <h2>
+                <i class="fa-solid fa-phone"></i> Teléfono
+              </h2>
+              <span class="info-contacto">11223344</span>
+            </div>
+            <div class="col">
+              <h2>
+                <i class="fa-solid fa-envelope"></i> Correo
+              </h2>
+              <span class="info-contacto">ethansarredondo@gmail</span>
+            </div>
+            <div class="col">
+              <h2>
+                <i class="fa-solid fa-globe"></i> Página Web
+              </h2>
+              <span class="info-contacto">ethanarredondo.com</span>
+            </div>
+          </div>
         </div>
       </section>
     </div>
